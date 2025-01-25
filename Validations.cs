bc99e1eacf5394c346cbe8a0bd79907996470508
Abazr Amin Hamza <azr032024@gmail.com>
2025-01-25 16:46:10 +0300

    Files
    Upload File

---
+++ Validations.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Form_Validations
+{
+    public class Froms_Validations
+    {
+        public static bool ValidateEmail(string email)
+        {
+            var pattern = @"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+
+            var regex = new Regex(pattern);
+
+            return regex.IsMatch(email);
+
+        }
+
+
+        public static bool ValidateInteger(string Number)
+        {
+            var Pattern = @"^[0-9]+$";
+
+            var regex = new Regex(Pattern);
+
+            return regex.IsMatch(Pattern);
+        }
+
+
+        public static bool ValidateFloat(string Number)
+        {
+            var pattern = @"^[0-9]*(?:\.[0-9]*)?$";
+
+            var regex = new Regex(pattern);
+
+            return regex.IsMatch(Number);
+        }
+
+        public static bool IsNumber(string Number)
+        {
+            return (ValidateInteger(Number) || ValidateFloat(Number));
+        }
+
+    }
+}
